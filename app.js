f89860aacdeeab7729c17c7a8a2cfacd32781d58
istiaque ahemed <istiaquepersonal@gmail.com>
2021-08-18 19:40:39 +0600

    all complete

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
     const pinString = pin + '';
-    if (pinString.length == 4){
+    if (pinString.length == 4) {
         return pin
     }
-    else{
+    else {
         return getPin();
     }
 
@@ -13,11 +13,33 @@ function getPin() {
 function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
-}   
-document.getElementById('key-pad').addEventListener('click',function(event){
+
+}
+document.getElementById('key-pad').addEventListener('click', function (event) {
     const number = event.target.innerText;
     const caleInput = document.getElementById('typed-number');
-    const previusNumber = caleInput.value;
-    const newNumber = previusNumber + number;
-    caleInput.value = newNumber;
+    if (isNaN(number)) {
+        if (number == 'C') {
+            caleInput.value = '';
+        }
+    }
+    else {
+        const previusNumber = caleInput.value;
+        const newNumber = previusNumber + number;
+        caleInput.value = newNumber;
+    }
 })
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumber = document.getElementById('typed-number').value;
+    const successMessage = document.getElementById('notify-success');
+    const fialError = document.getElementById('notify-fial');
+    if (pin == typedNumber) {
+       successMessage.style.display = 'block';
+        fialError.style.display = 'none';
+    }
+    else {
+        successMessage.style.display = 'none';
+        fialError.style.display = 'block';
+    }
+}
